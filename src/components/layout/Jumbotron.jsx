3ba09d430e8a02f048740af1a040870bec2d4d39
unknown <SOPORTE@N-202-02.north.systems>
2020-12-16 15:32:17 -0400

    jumbotron terminado mobile

--- src/components/layout/Jumbotron.jsx
+++ src/components/layout/Jumbotron.jsx
@@ -5,6 +5,11 @@ import Facebook from '../../assets/facebook.png'
 import Linkedin from '../../assets/linkedin.png'
 import Instagram from '../../assets/instagram.png'
 import Github from '../../assets/github.png'
+import Whatsapp from '../../assets/whatsapp.png'
+
+import '../../cssResponsive/jumbotron.css'
+
+import {FaBars} from 'react-icons/fa'
 
 const Jumbotron = () => {
 
@@ -24,16 +29,20 @@ const Jumbotron = () => {
                     clearInterval(nuevoNombre)
                     setescribiendo(false);
                 }
-            }, 200);
+            }, 100);
         }
         writeName('DANIEL GUILLERMO GORIANZ FERRUFINO');
     }, [])
 
-
+    const mostrarMenu = () => {
+        document.querySelector('#nav').classList.toggle('active')
+    }
+    
     return ( 
         <div className="Jumbotron">
             <div className="container">
-                <nav>
+                <button onClick={mostrarMenu}><FaBars/></button>
+                <nav id="nav">
                     <ul>
                         <li>Personal</li>
                         <li>Estudio</li>
@@ -48,18 +57,25 @@ const Jumbotron = () => {
                     </figure>
                     <h1>{nombre} {escribiendo ? <span>|</span> :null}</h1>
                     <figure className="container-redes">
-                        <a href="https://www.facebook.com/danielgorianz10/" target="_blank">
+                        <a href="https://www.facebook.com/danielgorianz10/" target="_blank" rel="noreferrer">
                             <img src={Facebook} alt=""/>
                         </a>
-                        <a href="https://www.instagram.com/dan_gorianz/" target="_blank">
+                        <a href="https://www.instagram.com/dan_gorianz/" target="_blank" rel="noreferrer">
                             <img src={Instagram} alt=""/>
                         </a>
-                        <a href="https://www.linkedin.com/in/daniel-gorianz-ferrufino-296511184/" target="_blank">
+                        <a href="https://www.linkedin.com/in/daniel-gorianz-ferrufino-296511184/" target="_blank" rel="noreferrer">
                             <img src={Linkedin} alt=""/>
                         </a>
-                        <a href="https://github.com/jockeer" target="_blank">
+                        <a href="https://github.com/jockeer" target="_blank" rel="noreferrer">
                             <img src={Github} alt=""/>
                         </a>
+                        <a class="wap-web" data-action="open" data-phone="59176597228" data-message="" rel="noreferrer" href="https://web.whatsapp.com/send?phone=59176597228&amp;text=¡Hola Danie! Quisiera saber acerca de ti." target="_blank">
+                            <img src={Whatsapp} alt=""/>
+                        </a>
+                        <a class="wap-mobile" data-action="open" data-phone="59176597228" data-message="" rel="noreferrer" href="https://api.whatsapp.com/send?phone=59176597228&amp;text=¡Hola Danie! Quisiera saber acerca de ti." target="_blank">
+                            <img src={Whatsapp} alt=""/>
+                        </a>
+                        
                         
 
                     </figure>
